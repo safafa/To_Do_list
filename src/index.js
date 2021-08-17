25403360091d33bef6fc24c0345563d037f7d3b9
safafa <safaaballagh97@gmail.com>
2021-08-17 12:23:57 +0100

    Add list html

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
-import _ from 'lodash';
+import 'lodash';
 import './style.css';
 
+const tasks = [{
+  description: 'first task',
+  completed: false,
+  index: 0,
+},
+{
+  description: 'second task',
+  completed: false,
+  index: 1,
+},
+{
+  description: 'third task',
+  completed: false,
+  index: 2,
+}];
+
 function component() {
-  const element = document.createElement('div');
+  const element = document.createElement('ul');
+  tasks.forEach((task) => {
+    const { description, completed, index } = task;
+    const li = document.createElement('li');
+    li.innerHTML = `Task ${index}  ${description} is completed: ${completed}`;
+    element.appendChild(li);
+  });
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
   return element;
 }
 
-document.body.appendChild(component());
+const toDoList = document.getElementById('to_do_list');
+toDoList.appendChild(component());
+document.body.appendChild(toDoList);
