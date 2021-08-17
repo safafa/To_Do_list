54ddedcd85ac25cb5ba46ca48196c87efe630675
safafa <safaaballagh97@gmail.com>
2021-08-17 11:58:24 +0100

    Add linters

--- webpack.config.js
+++ webpack.config.js
@@ -5,23 +5,23 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   devServer: {
-      contentBase: './dist',
-      port: '8081',
+    contentBase: './dist',
+    port: '8081',
   },
   plugins: [
-   new HtmlWebpackPlugin({
-    title: 'Output Management',
-    template: './src/index.html'
-  }),
- ],
- module: {
-     rules: [
-         {
-             test: /\.css$/i,
-             use: ['style-loader','css-loader'],
-         },
-     ],
- },
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
